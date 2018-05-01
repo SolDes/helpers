4d7dd7d32b86e65c1bbec5548e17fbdfacd703c0
Daniel Ferro <dlcerva@gmail.com>
2018-05-01 13:57:37 -0400

    Update groupBy.js
    Fixed group by to only do by prop for now.

--- groupBy.js
+++ groupBy.js
@@ -1,17 +1,8 @@
 module.exports = (collection, iteratee) => {
-  return collection.reduce((rv, x) => {
-    const v = iteratee instanceof Function ? iteratee(x) : x[iteratee];
-    const el = rv.find(r => r && r.key === v);
-
-    if (el) {
-      el.values.push(x);
-    } else {
-      rv.push({
-        key: v,
-        values: [x]
-      });
-    }
-    return rv;
-  }, []);
+  return collection.reduce((groups, item) => {
+    const val = item[prop];
+    groups[val] = groups[val] || [];
+    groups[val].push(item);
+    return groups;
+  }, {});
 };
-
